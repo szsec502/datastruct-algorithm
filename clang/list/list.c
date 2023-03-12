473236629a3c44e2c303b161cfa8c64bd2543f54
seaung <seaung.c0m7de@protonmail.com>
2023-03-13 00:45:22 +0800

    为线性表添加readme文件，并对c文件添加注释

--- clang/list/list.c
+++ clang/list/list.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+ * 1. 创建线性表
+ * 2. 为线性表分配内存
+ * 3. 初始化线性表成员
+ * 对于线性表L我们应将L队友的内存置为空
+ * 对于线性表L的长度应置为-1
+ * 最后返回线性表
+ * */
 ptrlink new_link()
 {
 	ptrlink L; // 创建线性表
@@ -20,6 +28,11 @@ ptrlink new_link()
 	return L;
 }
 
+/*
+ * 释放线性表的前提是线性表不能为NULL
+ * 使用free(L)释放线性表内存
+ * 最后将线性表L的值置为NULL，并返回零表示释放成功
+ * */
 int list_free(ptrlink L)
 {
 	if (L == NULL) // 如果L为NULL直接返回
@@ -35,6 +48,8 @@ int list_free(ptrlink L)
 
 /*
  * list_clear是将线性表里的内容清空，而不是删除线性表本身
+ * 清空线性表的前提是，线性表L不能为NULL
+ * 清空的操作就是将线性表L所占用的内存置空，并将线性表的长度置为负一,最后返回零表示清空成功
  * */
 int list_clear(ptrlink L)
 {
@@ -49,6 +64,9 @@ int list_clear(ptrlink L)
 	return 0;
 }
 
+/*
+ * 判断线性表是否为空只需判断线性表的长度last是否为-1
+ * */
 int list_empty(ptrlink L)
 {
 	if (L->last == -1)
@@ -59,15 +77,21 @@ int list_empty(ptrlink L)
 	return 0;
 }
 
+/*
+ * 获取线性表的长度的前提是线性表L不能为空，我们只需将线性表的长度last返回即可
+ * */
 int list_length(ptrlink L)
 {
 	if (L == NULL)
 	{
 		return -1;
 	}
-	return (L->last + 1);
+	return (L->last + 1); // 这里需要加一的原因是，线性表的下表是从零开始的所以得加一
 }
 
+/*
+ * 判断某个元素释放存在与线性表当中，其实就是使用for循环对线性表每个值跟指定的值进行比较如果相等就返回线性表元素的位置
+ * */
 int list_localte(ptrlink L, data_t value)
 {
 	int i;
@@ -81,6 +105,13 @@ int list_localte(ptrlink L, data_t value)
 	return -1;
 }
 
+/*
+ * 向线性表中插入指定位置的元素
+ * 1. 判断线性表是否满了
+ * 2. 校验位置参数是否合法pos必须大于0,或者pos大于线性表长度-1
+ * 3. 移动线性表，循环条件是i>=pos, 移动使用L->last的位置开始移动L->value[i + 1] = L->value[i]
+ * 4. 元素移动完成后，将带插入的值，放到指定的位置上(L-value[pos] = value), 线性表长度加加(L->last++)
+ * */
 int list_insert(ptrlink L, data_t value, int pos)
 {
 	int i;
@@ -158,6 +189,9 @@ int list_show(ptrlink L)
 	return 0;
 }
 
+/*
+ * 合并线性表就是对线性表进行遍历，如L2中的某个元素存在与L1中则将L2中的元素加入到L1中
+ * */
 int list_merge(ptrlink L1, ptrlink L2)
 {
 	int i = 0;
@@ -176,6 +210,12 @@ int list_merge(ptrlink L1, ptrlink L2)
 	return 0;
 }
 
+/*
+ * 线性表的去重操作就是将线性表两两相邻的两个元素进行比较，如果他们相等则进行删除操作
+ * 1. 判断线性表是否为空
+ * 2. 外层循环控制线性表中前一个元素，内层循环控制线性表后一个元素
+ * 3. 如果相邻的两个元素不相等，则j--, 如果j小于零了i++
+ */
 int list_purge(ptrlink L)
 {
 	int i, j;
--- clang/list/list.h
+++ clang/list/list.h
@@ -3,34 +3,78 @@
 
 #define MAX_LEN 10
 
-typedef int data_t;
+/*
+ * 线性表的线性存储
+ * 定义存储数据类型数据
+ * */
+typedef int data_t; // data_t为int的别名
 
+
+/*
+ * 线性表的结构
+ * 类型为data_t的value数组用于存储线性表数据元素
+ * last用于存放线性表长度
+ * */
 typedef struct
 {
 	data_t value[MAX_LEN];
 	int last;
 }list, *ptrlink;
 
+/*
+ * 用于初始化线性表
+ * 返回值为指针类型的ptrlink
+ * */
 ptrlink new_link();
 
+/*
+ * 释放线性表内存,删除线性表
+ * */
 int list_free(ptrlink L);
 
+/*
+ * 清空线性表内容，不删除线性表
+ * */
 int list_clear(ptrlink L);
 
+/*
+ * 判断线性表是否为空
+ * */
 int list_empty(ptrlink L);
 
+/*
+ * 获取线性表长度
+ * */
 int list_length(ptrlink L);
 
+/*
+ * 判断某个给定的值是否存在于线性表中
+ * */
 int list_localte(ptrlink L, data_t value);
 
+/*
+ * 给定指定位置往线性表插入元素
+ * */
 int list_insert(ptrlink L, data_t value, int pos);
 
+/*
+ * 删除指定位置的线性表元素
+ * */
 int list_delete(ptrlink L, int pos);
 
+/*
+ * 打印输出线性表
+ * */
 int list_show(ptrlink L);
 
+/*
+ * 合并线性表
+ * */
 int list_merge(ptrlink L1, ptrlink L2);
 
+/*
+ * 线性表去重
+ * */
 int list_purge(ptrlink L);
 
 
